da615a52b507229d0f710eaf510eb49761c2cf31
maitri2705 <maitri2705@gmail.com>
2017-11-08 01:08:07 -0600

    Both extraction are done

--- src/ImageExtraction.py
+++ src/ImageExtraction.py
@@ -1,6 +1,6 @@
 import cv2
-
-img=cv2.imread("lady.jpeg")
+#img=cv2.imread("C://Users//MAITRI SHAH//PycharmProjects//MLProject//kaggleImageNet//img//lady.jpeg")
+img=cv2.imread("../img/lady.jpeg")
 out=img[246:320,132:173]
 cv2.imwrite("Cropped.jpeg",out)
 cv2.waitKey(0)
